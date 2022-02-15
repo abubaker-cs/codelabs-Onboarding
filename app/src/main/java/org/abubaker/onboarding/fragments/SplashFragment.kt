0a3f61074f6c3c6caf354c88ed679046bc4f6fec
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-02-16 03:48:53 +0500

    Working...

--- app/src/main/java/org/abubaker/onboarding/fragments/SplashFragment.kt
+++ app/src/main/java/org/abubaker/onboarding/fragments/SplashFragment.kt
@@ -18,15 +18,35 @@ class SplashFragment : Fragment() {
     ): View? {
 
         Handler().postDelayed({
-            findNavController().navigate(R.id.action_splashFragment_to_viewPagerFragment)
+
+            // If the finished value is TRUE, then we will skip the ViewPager (instructions) and move
+            // the user directly to the HOME screen
+            if (onBoardingFinished()) {
+
+                // Skip the Viewpager and move the user directly to the Home page
+                findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+
+
+            } else {
+
+                // Start ViewPager to let the user see on-boarding instructions
+                findNavController().navigate(R.id.action_splashFragment_to_viewPagerFragment)
+
+            }
+
         }, 3000)
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false)
     }
 
+
     private fun onBoardingFinished(): Boolean {
+
+        // Get the onBoarding parameter from the SharedPreferences
         val sharedPref = requireActivity().getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
+
+        // Reset the value of Finished to False
         return sharedPref.getBoolean("Finished", false)
     }
 
--- app/src/main/java/org/abubaker/onboarding/fragments/onboarding/screens/ThirdScreen.kt
+++ app/src/main/java/org/abubaker/onboarding/fragments/onboarding/screens/ThirdScreen.kt
@@ -7,6 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import org.abubaker.onboarding.R
 import org.abubaker.onboarding.databinding.FragmentThirdScreenBinding
 
 
@@ -25,25 +29,28 @@ class ThirdScreen : Fragment() {
         // Button: Finish
         mBinding.btnFinish.setOnClickListener {
 
-
-            // Navigation.findNavController()
-            // findNavController.navigate(R.id.action_viewPagerFragment_to_homeFragment)
-
-            // navController = Navigation.findNavController( this, R.id.action_viewPagerFragment_to_homeFragment)
-            // NavigationUI.setupActionBarWithNavController(this, navController )
-
-
+            findNavController().navigate(R.id.action_viewPagerFragment_to_homeFragment)
             onBoardingFinished()
+
         }
 
         return mBinding.root
     }
 
     private fun onBoardingFinished() {
+
+        // Pass onBoarding to the sharedPref
         val sharedPref = requireActivity().getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
+
+        // Open Editor
         val editor = sharedPref.edit()
+
+        // We are using Boolean to define the current state of the process
         editor.putBoolean("Finished", true)
+
+        //
         editor.apply()
+
     }
 
 }
